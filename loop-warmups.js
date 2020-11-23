cd9d0c4242f95d72d88376dd9a77db893549ebd1
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-11-23 15:17:58 +0100

    uppdrag 1

---
+++ loop-warmups.js
@@ -0,0 +1,16 @@
+//let x = 10
+
+//while (x > 3) {
+//    console.log(x)
+//    x--
+//}
+
+//console.log(x)
+
+
+function myfunc (arr) {
+    for (let i = 0; i < arr.length; i++) {
+    console.log(i)
+    }
+}
+myfunc([3, 4, 9])
